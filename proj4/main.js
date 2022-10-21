10b26e9295cb05298c7b4aafb2e90ce69f72d82a
udagea <114105311+udagea@users.noreply.github.com>
2022-10-21 00:05:30 -0700

    Update main.js

--- proj4/main.js
+++ proj4/main.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   
-  $("#background").click(function() {
-    $("p").css("color", "green");
-    //$("img").attr("src", "images/dark.gif");
-  });
-  
   $("img").click(function(){
     // Change src attribute of image
     $(background).attr("src", "images/dark.gif");
-    $("p").css("color", "green");
+    $("p").css("color", "gray");
+    $("p").css({"font-family": "Arial, Helvetica, sans-serif"});
+    $("h1").css({"font-family": "Arial, Helvetica, sans-serif", "font-size": "200%"});
+    $("#p2").show();
+    $("#p3").show();
   });
-
+  
+  $("#p3").click(function(){
+    // Change src attribute of image
+    $(background).attr("src", "images/house.gif");
+    alert("How Interesting.");
+  });
 });
 ''
