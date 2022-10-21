8d8f54daa9d3a86d9d5c962cb74d317c25f0cdb4
udagea <114105311+udagea@users.noreply.github.com>
2022-10-21 11:10:43 -0700

    Update main.js

--- proj4/main.js
+++ proj4/main.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   $("img").click(function(){
     // Change src attribute of image
     $(background).attr("src", "images/dark.gif");
-    $("p").css("color", "white");
+    $("p").css("color", "gray");
     $("p").css({"font-family": "Arial, Helvetica, sans-serif"});
     $("h1").css({"font-family": "Arial, Helvetica, sans-serif", "font-size": "200%"});
-     $("h1").css("color", "white");
+     $("h1").css("color", "gray");
     $("#p2").show();
     $("#p3").show();
     $("body").css("color", "black");
