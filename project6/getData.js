fe86b40b159d38fba2a3cdbf6c96e8640997b840
udagea <114105311+udagea@users.noreply.github.com>
2022-11-01 22:32:30 -0700

    Add files via upload

---
+++ project6/getData.js
@@ -0,0 +1,38 @@
+
+$.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+    var number = data['number'];
+    
+    var name =data['name'];
+
+    var craft=data['craft'];
+
+    var people=data['people'];
+
+    
+$.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+    var number = data['number'];
+    $('#SpacePeople').html(number);
+
+    data['people'].forEach(function (d) {
+        if (d['craft'] == 'Tiangong'){
+            $('#TiangongNames').append('<ul>' + d['name'] + '</ul>');
+        } else{
+            $('#ISSNames').append('<ul>' + d['name'] + '</ul>');
+        }
+    });
+});
+    }); 
+    
+    // data['people'].forEach(function (d) {
+        // $('#astroNames').append('<li>' + d['name']+ '</li>');
+        // $('#TiangongNames').append('<li>' + d['name']+ '</li>');
+        //     if ("craft" = "Tiangong") {
+        //     return true;
+        // }
+        // $('#ISSNames').append('<li>' + d['name']+ '</li>');
+        //     if ("craft" = "ISS") {
+        //     return true;
+        // }
+        // });
+        
+       
